5d0a02291acbce04c9f368dc54672b13289eb401
Guilherme Martins <guilherme.mleal.11@gmail.com>
2024-04-16 21:33:46 -0300

    feat(product): implemented  product and not controller

--- src/main/java/com/br/martins/market_company/dto/ProductsDTO.java
+++ src/main/java/com/br/martins/market_company/dto/ProductsDTO.java
@@ -1,4 +1,8 @@
 package com.br.martins.market_company.dto;
 
-public record ProductsDTO() {
+import com.br.martins.market_company.models.MarketSectorEntity;
+
+public record ProductsDTO(int id_product, MarketSectorEntity sector_market,
+                          String name_product, double price_product,
+                          int quantity_product) {
 }
--- src/main/java/com/br/martins/market_company/models/ProductsEntity.java
+++ src/main/java/com/br/martins/market_company/models/ProductsEntity.java
@@ -1,11 +1,42 @@
 package com.br.martins.market_company.models;
 
+import com.br.martins.market_company.dto.MarketSectorDTO;
+import com.br.martins.market_company.dto.ProductsDTO;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Products_Table")
 public class ProductsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_product;
+
+    @ManyToOne
+    @JoinColumn(name = "sector_id")
+    private MarketSectorEntity sector_market;
+
+    @Column(name = "PRODUCT_NAME")
+    private String name_product;
+
+    @Column(name="PRODUCT_PRICE")
+    private double price_product;
+
+    @Column(name = "PRODUCT_QUANTITY")
+    private int quantity_product;
+
+    public ProductsEntity(ProductsDTO dto_entity){
+        this.id_product = dto_entity.id_product();
+        this.sector_market = dto_entity.sector_market();
+        this.quantity_product = dto_entity.quantity_product();
+        this.name_product = dto_entity.name_product();
+        this.price_product = dto_entity.price_product();
+    }
 }
