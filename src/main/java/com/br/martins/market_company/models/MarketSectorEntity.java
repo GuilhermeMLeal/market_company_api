45286cad3e026d647ca87a29af2211d5bce89a6e
Guilherme Martins <guilherme.mleal.11@gmail.com>
2024-04-16 21:34:35 -0300

    feat(sector_market): implemented sector on the market

--- src/main/java/com/br/martins/market_company/models/MarketSectorEntity.java
+++ src/main/java/com/br/martins/market_company/models/MarketSectorEntity.java
@@ -1,17 +1,18 @@
 package com.br.martins.market_company.models;
 
 import com.br.martins.market_company.dto.MarketSectorDTO;
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Sector_Table")
 public class MarketSectorEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
